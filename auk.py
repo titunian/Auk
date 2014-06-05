2567827211a6b3682b24d4b5badc0a8e54ddbb95
Phani Kumar <psp.phanikumar@gmail.com>
2014-06-06 02:53:37 +0530

    Reimplemented functions and code cleanup

--- auk.py
+++ auk.py
@@ -1,66 +1,53 @@
-import sys, soundcloud, pyen
+import sys
+import soundcloud
+import pyen
 
 #Configuration
 pyen_key = "8GDKECFTIJHEADTWC" #echonest
 client_id = "53188e4558d06691aac3cf57ef3a7cd7" #soundcloud
+# Setting things up
 en = pyen.Pyen(pyen_key)
 client = soundcloud.Client(client_id=client_id)
-def aukfetch(root_track, root_artist = None):
-	related_dict={}
-
-	# response = en.get('song/search', artist=root_artist, title=root_track, bucket=['audio_summary'], results=1)
-	
-	# for song in response['songs']:
-	# 	root_duration = song["audio_summary"]['duration']
-	# #print root_duration
-	# root_artist = song["artist_name"]
-	# root_track = song["title"]
-	# #handle exceptions for license
-	# print "root %-32.32s %s" % (root_artist, root_track)
-	# print sc_streamurl(root_track, root_artist, root_duration)
-	response = en.get('playlist/static', artist=root_artist, type='artist-radio', results=10)
 
 
-	for i, new_song in enumerate(response['songs']):
-		templist=[]
-		response2 = en.get('song/search', artist=new_song['artist_name'], title=new_song['title'], bucket=['audio_summary'], results=1)
-		for song in response2['songs']:
-			new__duration = song["audio_summary"]['duration']
-		#print "%d %-32.32s %s" % (i, new_song['artist_name'], new_song['title'])
-		#print sc_streamurl(new_song['title'], new_song['artist_name'], new__duration)
+# Dicts are resource heavy. Avoid them wherever possible.
 
-		templist.append(new_song['artist_name'])
-		templist.append(new_song['title'])
-		templist.append(sc_streamurl(new_song['title'], new_song['artist_name'], new__duration))
+def song_info(root_artist, root_track):
+	""" Takes in track/artist as arguments and returns a list with related information"""
+	templist=[]
+	track_response = en.get('song/search', artist=root_artist, title=root_track, bucket=['audio_summary'], results=1)
+	for track in track_response['songs']:
+		root_duration = track["audio_summary"]['duration']
 
-		related_dict[i] = templist
+	templist.append(root_artist)
+	templist.append(root_track)
+	templist.append(sc_streamurl(root_track, root_artist, root_duration))
 
-	return related_dict
+	return templist
+
+
+def aukfetch(size, root_track, root_artist = None):
+	"""Takes in track/(artist) and size as arguments and returns size number of related track info as a dict"""
+
+	## Including size as an argument helps in realizing dynamic playlists.
+	relatedtracks_response = en.get('playlist/static', artist=root_artist, type='artist-radio', results=size)
+	return relatedtracks_response
 
 
 def sc_streamurl(root_track, root_artist, root_duration):
-	"""
-	Gives back the soundcloud stream url for a given track input
-	"""
-	tracks = client.get('/tracks', q=root_artist+' '+root_track, duration={'from': root_duration-2,'to': root_duration+2}, results=3)
-	try:
-		#print tracks[0].title
-		su =tracks[0].stream_url
-		stream_url = client.get(su, allow_redirects=False)
+	"""Returns a soundcloud stream url for a given track input"""
 
-	# print the tracks stream URLnj
-		return stream_url.location
+	tracks = client.get('/tracks', q=root_artist+' '+root_track, duration={'from': root_duration-2,'to': root_duration+2}, results=3)
+
+	try:
+		su =tracks[0].stream_url
+		surl = client.get(su, allow_redirects=False)
+		return surl.location
 	except:
-		#print tracks[1].title
 		try:
 			su =tracks[1].stream_url
-			stream_url = client.get(su, allow_redirects=False)
-			return stream_url.location
+			surl = client.get(su, allow_redirects=False)
+			return surl.location
 		except:
+			#print "[DEBUG]: Stream URL not found"
 			pass
-
-# print "Welcome to auk-0.1","\n"
-# root_track= raw_input("Enter track name:  ")
-# root_artist = raw_input("Enter artist name: ")
-
-# auk(root_track, root_artist)
